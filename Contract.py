4014b650c08c17ff98fcf23baddb722f3abfb5e5
qrisma <nobel77.lee@gmail.com>
2020-01-07 15:26:09 +0900

    연락처 출력하기

--- Contract.py
+++ Contract.py
@@ -27,16 +27,34 @@ def set_contact():
 
 
 def print_menu():
-    '''
+    """
     show menu and get input
     :return: returns menu id
-    '''
+    """
+
     print("1. 연락처 입력")
     print("2. 연락처 출력")
     print("3. 연락서 삭제")
     print("4. 종료")
 
-    return int(input("메뉴선택 : "))
+    menu = input("메뉴선택 : ")
+    try:
+        menu = int(menu)
+    except ValueError:
+        print("메뉴 아이디 변환 오류")
+
+    return menu
+
+
+def print_contact(contact_list):
+    """
+    prints contact list
+
+    :param contact_list: 
+    :return: None
+    """
+    for contact in contact_list:
+        contact.print_info()
 
 
 def run():
@@ -45,11 +63,17 @@ def run():
     while 1:
         # getting input
         menu = print_menu()
-        if menu ==1:
+        if menu == 1:
             contact = set_contact();
             contact_list.append(contact)
-        if menu == 4:
-            break;
+        elif menu == 2:
+            print_contact(contact_list)
+        elif menu == 3:
+            break
+        elif menu == 4:
+            break
+        else:
+            print("잘못 입력하였습니다. 메뉴를 다시 입력하세요")
 
 
 if __name__ == "__main__":
